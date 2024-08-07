5d0be4048b1d7beb0e589dff8be234e692293408
Anand Vyas <anand.vyas@globalgarner.com>
2024-08-07 19:04:12 +0530

    new initial setup

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function App() {
     return (
         <View>
-            <StatusBar style="auto" />
+            <StatusBar style="auto" animated />
         </View>
     );
 }
