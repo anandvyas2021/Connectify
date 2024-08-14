140e19b6240dc074086cb934f6bd637bf1609055
Anand Vyas <anand.vyas@globalgarner.com>
2024-08-14 17:18:55 +0530

    setting bottom tab styles and functionality

--- src/navigation/TabNavigation.jsx
+++ src/navigation/TabNavigation.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-import { BlurView } from "expo-blur";
-import { Ionicons, Feather } from "@expo/vector-icons";
+// import { BlurView } from "expo-blur";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import Home from "../screens/MainApp/BottomTabs/Home";
@@ -10,7 +11,6 @@ import Plus from "../screens/MainApp/BottomTabs/Plus";
 import Inbox from "../screens/MainApp/BottomTabs/Inbox";
 import Profile from "../screens/MainApp/BottomTabs/Profile";
 import { withSafeArea } from "../../common/SafeAreaWrapper";
-import { View, Text, StyleSheet } from "react-native";
 
 export default function TabNavigation(props) {
     const Tab = createBottomTabNavigator();
@@ -20,63 +20,80 @@ export default function TabNavigation(props) {
             name: "Home",
             key: "home",
             component: Home,
-            iconName: "home",
+            icon: "home-sharp",
+            focusedIcon: "home-outline",
         },
         {
             name: "Chat",
             key: "message-circle",
             component: Task,
-            iconName: "message-circle",
+            icon: "chatbox-ellipses-sharp",
+            focusedIcon: "chatbox-ellipses-outline",
         },
         {
             name: "Plus",
             key: "plus-circle",
             component: Plus,
-            iconName: "plus-circle",
+            icon: "add-circle-sharp",
+            focusedIcon: "add-circle-outline",
         },
         {
             name: "Inbox",
             key: "notifications",
             component: Inbox,
-            iconName: "notifications",
+            icon: "notifications-sharp",
+            focusedIcon: "notifications-outline",
         },
         {
             name: "Profile",
             key: "person",
             component: Profile,
-            iconName: "user",
+            icon: "person-circle-sharp",
+            focusedIcon: "person-circle-outline",
         },
     ];
+
     return (
         <Tab.Navigator
             screenOptions={({ route }) => {
                 const tabConfig = tabScreens?.find(
                     (tab) => tab?.name === route?.name
                 );
+                const isPlus = route?.name === "Plus";
+
                 return {
                     headerShown: false,
+                    tabBarInactiveTintColor: "#a2a5a8",
+                    tabBarActiveTintColor: "#016ef0",
+                    tabBarStyle: styles.tabBar,
+                    tabBarItemStyle: styles.tabItem,
                     tabBarIcon: ({ focused, color, size }) => {
                         return (
-                            <Feather
-                                name={`${tabConfig?.iconName}${
-                                    focused ? "" : ""
-                                }`}
-                                size={28}
+                            <Ionicons
+                                name={
+                                    focused
+                                        ? tabConfig?.icon
+                                        : tabConfig?.focusedIcon
+                                }
+                                size={isPlus ? 40 : focused ? 30 : size}
                                 color={color}
                             />
                         );
                     },
-                    tabBarShowLabel: false,
-                    tabBarInactiveTintColor: "#a2a5a8",
-                    tabBarActiveTintColor: "#006fef",
-                    tabBarStyle: {
-                        height: 60,
-                        margin: 13,
-                        position: "absolute",
-                        borderRadius: 25,
-                        borderWidth: 1,
-                        borderColor: "#d1d5db",
+                    tabBarIconStyle: isPlus ? { ...styles.tabIcon1 } : null,
+                    // tabBarShowLabel: false,
+                    tabBarLabel: ({ focused, color }) => {
+                        return isPlus ? null : (
+                            <Text
+                                style={{ color, fontSize: 12, fontWeight: 700 }}
+                            >
+                                {route?.name}
+                            </Text>
+                        );
                     },
+                    tabBarBadge: route?.name === "Chat" ? 2 : null,
+                    tabBarBadgeStyle: styles?.tabBadge,
+
                     // tabBarBackground: () => (
                     //     <BlurView
                     //         intensity={50}
@@ -84,16 +101,6 @@ export default function TabNavigation(props) {
                     //         style={{ flex: 1 }}
                     //     />
                     // ),
-                    // tabBarBackground: () => (
-                    //     <View className="bg-white absolute bottom-0 h-full w-full">
-                    //         <BlurView
-                    //             // style={{ height: 100 }}
-                    //             blurType="light"
-                    //             blurAmount={10}
-                    //         ></BlurView>
-                    //     </View>
-                    // ),
-                    tabBarLabelStyle: { fontSize: 12, fontWeight: 700 },
                 };
             }}
         >
@@ -102,25 +109,37 @@ export default function TabNavigation(props) {
                     key={tab?.key}
                     name={tab?.name}
                     component={withSafeArea(tab?.component)}
+                    options={
+                        {
+                            //     tabBarButton: (props) =>
+                            //         tab?.name === "Plus" ? (
+                            //             <CustomTabBarButton {...props} />
+                            //         ) : null,
+                        }
+                    }
                 />
             ))}
         </Tab.Navigator>
     );
 }
 
-{
-    /* <Tab.Screen
-                name="Home"
-                key="home"
-                component={Home}
-                options={{
-                    tabBarIcon: ({ color, size }) => (
-                        <Ionicons
-                            name={`home-${iconType}`}
-                            color="#ef5589"
-                            size={25}
-                        />
-                    ),
-                }}
-            />  */
-}
+const styles = StyleSheet.create({
+    tabBar: {
+        minHeight: 60,
+        overflow: "hidden",
+        margin: 15,
+        borderRadius: 15,
+        borderWidth: 1,
+        borderColor: "#d1d5db",
+    },
+    tabItem: {
+        paddingBottom: 5,
+        paddingTop: 5,
+        paddingHorizontal: 2,
+    },
+    tabIcon1: { top: -0, zIndex: 100 },
+    tabBadge: {
+        fontSize: 13,
+        fontWeight: 800,
+    },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {
-            colors: { pink: "#ef5589", bottomTabText: "", bottomTabActive: "" },
+            colors: {
+                pink: "#ef5589",
+                blueLight1: "#e1edfa",
+                bottomTabText: "",
+                bottomTabActive: "",
+            },
         },
     },
     plugins: [],
